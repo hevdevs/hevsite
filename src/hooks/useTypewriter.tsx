bf24d01a3679505582b33d6d74b6d44be77e2957
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2024-12-08 21:20:56 +0000

    add typewriter hook for animating text

---
+++ src/hooks/useTypewriter.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect, useMemo } from "react";
+
+const useTypewriter = (text: String[], speed = 120) => {
+	const [charIndex, setCharIndex] = useState(0);
+	const [dialogueIndex, setDialogueIndex] = useState(0);
+	const [isTyping, setIsTyping] = useState(true);
+
+	const displayText = useMemo(
+		() => text[dialogueIndex].slice(0, charIndex),
+		[charIndex]
+	);
+	function nextLine() {
+		text[dialogueIndex + 1]
+			? setDialogueIndex((i) => i + 1)
+			: setDialogueIndex(0);
+		setCharIndex(0);
+		setIsTyping(true);
+	}
+	useEffect(() => {
+		if (charIndex === text[dialogueIndex].length) {
+			setIsTyping(() => false);
+			return;
+		}
+		const timeoutId = setTimeout(() => {
+			setCharIndex((i) => i + 1);
+		}, speed);
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	}, [charIndex, dialogueIndex, speed]);
+
+	return { nextLine, displayText, isTyping };
+};
+
+export default useTypewriter;
