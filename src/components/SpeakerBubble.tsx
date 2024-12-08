bcadd8e3fbac5b51caa25a2fb3d643f3618f82a4
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2024-12-08 21:21:35 +0000

    add dialogue text

--- src/components/SpeakerBubble.tsx
+++ src/components/SpeakerBubble.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import useTypewriter from "../hooks/useTypewriter";
+import DialogueBox from "./DialogueBox";
 
 const SpeakerBubble: React.FC = () => {
-	return <div className="speaker-bubble">SpeakerBubble</div>;
+	const { nextLine, isTyping, displayText } = useTypewriter([
+		"Hi there! My name is Hev. It's nice to meet you.",
+		"I am a Software Engineer and Educator, currently based in Manchester.",
+		"Fancy a look around...?",
+	]);
+
+	return (
+		<div className="speaker-bubble">
+			<span className="cursor">
+				<p>{displayText}</p>
+			</span>
+			<button disabled={isTyping} onClick={nextLine}>
+				Continue
+			</button>
+			<DialogueBox />
+		</div>
+	);
 };
 
 export default SpeakerBubble;
