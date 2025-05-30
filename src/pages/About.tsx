30f9b87209611862b370d786c6cfcb54d253e6af
hevdevs <heathermagnier@outlook.com>
2025-05-30 20:35:49 +0100

    add sprites

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import DialogueBox from "../components/DialogueBox";
 import SpeakerBubble from "../components/SpeakerBubble";
+import hevIdle from "../assets/hev_sprite_3.gif"
+import hevGiggle from "../assets/hev_sprite_2.gif"
+import { useState } from "react";
 
-const About: React.FC = () => {
+const About = () => {
+	const [hover, setHover] = useState(false)
+	function handleMouseOver (){
+		setHover(true)
+	}
+	function handleMouseLeave(){
+		setHover(false)
+	}
 	return (
 		<div className="portal">
-			<SpeakerBubble />
-			<div className="placeholder" />
-			<DialogueBox />
+			<SpeakerBubble dialogue={["Here's some more about me"]} />
+			<img className="sprite" onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave} src={hover ? hevGiggle : hevIdle}/>
 		</div>
 	);
 };
