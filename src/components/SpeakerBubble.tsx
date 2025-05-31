0d0b0a97aecdad4a6caae1bde033919856784884
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-05-31 17:45:39 +0100

    add consistent stlying to next dialogue button

--- src/components/SpeakerBubble.tsx
+++ src/components/SpeakerBubble.tsx
@@ -1,18 +1,20 @@
 import useTypewriter from "../hooks/useTypewriter";
 
-const SpeakerBubble  = ({dialogue}: {dialogue: String[]}) => {
+const SpeakerBubble = ({ dialogue }: { dialogue: String[] }) => {
 	const { nextLine, isTyping, displayText } = useTypewriter(dialogue);
 
 	return (
-		<div>
 		<div className="speaker-bubble">
-			<span className="cursor">
-				<p>{displayText}</p>
-			</span>
-			</div>
-			<button disabled={isTyping} onClick={nextLine}>
+			<p className="jersey-10-regular dialogue">
+				{displayText ? displayText : "..."}
+			</p>
+			<button
+				className={"dialogue-next"}
+				disabled={isTyping}
+				onClick={nextLine}>
 				Continue
-			</button></div>
+			</button>
+		</div>
 	);
 };
 
