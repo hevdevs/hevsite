17a86600a491bf1a2a6057842f5180193a432068
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-05-31 17:44:59 +0100

    swap out text for icons

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,24 @@
-import {NavLink } from "react-router";
+import { NavLink } from "react-router";
+import projects from "../assets/projects.svg";
+import home from "../assets/home.svg";
+import contact from "../assets/contact.svg";
+import briefcase from "../assets/briefcase.svg";
 
 const TopBar = () => {
 	return (
 		<section className="options-bar">
-			<NavLink to="/">Intro</NavLink>
-			<NavLink to="/work">XP</NavLink>
-			<NavLink to="/projects">Inventory</NavLink>
-			<NavLink to="/about">Character</NavLink>
+			<NavLink to="/contact" title="Contact Me" className="nav">
+				<img className="icon" src={contact} />
+			</NavLink>
+			<NavLink to="/projects" title="Projects" className="nav">
+				<img src={projects} className="icon" />
+			</NavLink>
+			<NavLink to="/work" title="Career" className="nav">
+				<img src={briefcase} className="icon" />
+			</NavLink>
+			<NavLink to="/" title="Home Page" className="nav">
+				<img src={home} className="icon" />
+			</NavLink>
 		</section>
 	);
 };
