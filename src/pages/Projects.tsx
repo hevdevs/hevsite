2617d8cc56722d1041e3f07059dca809f00a11ba
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-05-31 17:46:58 +0100

    format with prettier

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,19 +1,19 @@
 import SpeakerBubble from "../components/SpeakerBubble";
-import hevWorkStart from "../assets/hev_sprite_4.gif"
-import hevWorkLoop from "../assets/hev_sprite_5.gif"
+import hevWorkStart from "../assets/hev_sprite_4.gif";
+import hevWorkLoop from "../assets/hev_sprite_5.gif";
 import { useEffect, useState } from "react";
 
 const Projects = () => {
-	const [gifSrc, setGifSrc] = useState(hevWorkStart)
-	useEffect(()=>{
-		setTimeout(()=>{
-			setGifSrc(hevWorkLoop)
-		},3500)
-	},[])
+	const [gifSrc, setGifSrc] = useState(hevWorkStart);
+	useEffect(() => {
+		setTimeout(() => {
+			setGifSrc(hevWorkLoop);
+		}, 3500);
+	}, []);
 	return (
 		<div className="portal">
-			<SpeakerBubble dialogue={["Here are some of my recent projects"]}/>
-			<img src={gifSrc} className="sprite"/>
+			<SpeakerBubble dialogue={["Here are some of my recent projects"]} />
+			<img src={gifSrc} className="sprite" />
 		</div>
 	);
 };
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,8 +1,7 @@
 import SpeakerBubble from "../components/SpeakerBubble";
-import hevWork from "../assets/hev_sprite_4.gif"
+import hevWork from "../assets/hev_sprite_4.gif";
 
 const Work = () => {
-
 	return (
 		<div className="portal">
 			<SpeakerBubble dialogue={["Here's my recent work history"]} />
