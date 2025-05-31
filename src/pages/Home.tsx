a35862c3321d9616ad0194544f2a502cdd2917ff
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-05-31 17:46:36 +0100

    tweak home page dialogue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 import SpeakerBubble from "../components/SpeakerBubble";
-import hevHi from "../assets/hev_sprite_1.gif"
+import hevHi from "../assets/hev_sprite_1.gif";
 
 const Home = () => {
 	return (
 		<div className="portal">
-			<SpeakerBubble dialogue={[
-		"Hi there! My name is Hev. It's nice to meet you.",
-		"I am a Senior Software Engineer based in Manchester.",
-		"Fancy a look around...?",
-	]} />
-			<img src={hevHi} className="sprite"/>
+			<SpeakerBubble
+				dialogue={[
+					"Hi there! My name is Hev. It's nice to meet you.",
+					"I am a Senior Software Engineer based in Manchester.",
+					"Take a look around...",
+				]}
+			/>
+			<img src={hevHi} className="sprite" />
 		</div>
 	);
 };
