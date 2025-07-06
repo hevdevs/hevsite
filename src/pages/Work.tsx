672bd8c395722fe084510a69a3e01fe7d1632cd5
hevdevs <90092645+hevdevs@users.noreply.github.com>
2025-07-06 13:45:10 +0100

    update work page layout

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,22 +1,30 @@
 import SpeakerBubble from "../components/SpeakerBubble";
-import hevWork from "../assets/hev_sprite_4.gif";
+import hevTalk from "../assets/hev_sprite_6.gif";
 
 const Work = () => {
 	return (
 		<div className="portal">
-			<SpeakerBubble dialogue={["Here's my recent work history"]} />
+			<div className="alt-dialogue">
+				<img src={hevTalk} className="sprite-alt" id="talking-head" />
+				<SpeakerBubble dialogue={["Here's my recent work history"]} />
+			</div>
 			<div className="content-display">
-				<img src={hevWork} className="sprite" />
-				<div className="aside jersey-10-regular">
+				<div className="aside jersey-10-regular" id="cv">
 					<h2>Experience</h2>
-					<h3>Senior Software Engineer</h3>
-					<h4>Made Tech</h4>
-					<h3>Software Engineer and Lead Lecturer</h3>
-					<h4>Northcoders</h4>
-					<h3>Software Engineer and Seminar Lead</h3>
-					<h4>Northcoders</h4>
-					<h3>Junior Software Engineer and Mentor</h3>
-					<h4>Northcoders</h4>
+
+					<h3>
+						<a href="https://www.madetech.com/">Made Tech</a>
+					</h3>
+
+					<h4>Senior Software Engineer</h4>
+					<p></p>
+					<h3>
+						<a href="https://www.northcoders.com/">Northcoders</a>
+					</h3>
+					<h4>Software Engineer and Lead Lecturer</h4>
+					<h4>Software Engineer and Seminar Lead</h4>
+
+					<h4>Junior Software Engineer and Mentor</h4>
 				</div>
 			</div>
 		</div>
